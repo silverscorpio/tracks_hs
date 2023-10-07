602dea7ad8c37fa835146d47798120a147756474
silverscorpio <rajat.newtoneinstein@gmail.com>
2023-10-07 03:13:36 +0200

    pwd check abstracted out in a func

--- python_core/Password Hacker (Python)/Password Hacker (Python)/task/hacking/hack.py
+++ python_core/Password Hacker (Python)/Password Hacker (Python)/task/hacking/hack.py
@@ -8,23 +8,29 @@ from login_pwd_data import (ELEMENTS_LIST,
 from socket_conn import socket_operation
 
 
+def pwd_check(socket_conn: socket) -> str | None:
+    for pwd in passwords:
+        if pwd.isalpha():
+            pwd_combos = gen_case_combos_for_word(pwd)
+            for pwd_combo in pwd_combos:
+                response = socket_operation(socket_connection=socket_conn,
+                                            msg_to_send=pwd_combo)
+                if verify_pwd_crack(msg=response):
+                    return pwd_combo
+        response = socket_operation(socket_connection=socket_conn,
+                                    msg_to_send=pwd)
+        if verify_pwd_crack(msg=response):
+            return pwd
+
+
 def main():
     inputs_cmdline = get_cmdline_args()
     server_data = (inputs_cmdline.ipaddress, inputs_cmdline.port)
     with socket.socket() as client_socket:
         client_socket.connect(server_data)
-        for pwd in passwords:
-            if pwd.isalpha():
-                pwd_combos = gen_case_combos_for_word(pwd)
-                for pwd_combo in pwd_combos:
-                    response = socket_operation(socket_connection=client_socket,
-                                                msg_to_send=pwd_combo)
-                    if verify_pwd_crack(msg=response):
-                        return pwd_combo
-            response = socket_operation(socket_connection=client_socket,
-                                        msg_to_send=pwd)
-            if verify_pwd_crack(msg=response):
-                return pwd
+        cracked_pwd = pwd_check(socket_conn=client_socket)
+        if cracked_pwd is not None:
+            return cracked_pwd
 
 
 if __name__ == "__main__":
