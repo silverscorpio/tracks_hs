ea449648ffee0056ba99764678b7e012fed972ab
silverscorpio <rajat.newtoneinstein@gmail.com>
2023-10-08 04:14:34 +0200

    debugging, quite close

--- python_core/Password Hacker (Python)/Password Hacker (Python)/task/hacking/hack.py
+++ python_core/Password Hacker (Python)/Password Hacker (Python)/task/hacking/hack.py
@@ -14,28 +14,25 @@ def check_creds(socket_conn: socket, creds_data: list[str], creds_type: str) -> 
         for val_combo in val_combos:
             if creds_type == "login":
                 cracked_creds[creds_type] = val_combo
-            response = socket_operation(socket_connection=socket_conn,
-                                        msg_to_send=json.dumps(cracked_creds))
-            if creds_type == "login":
+                response = socket_operation(socket_connection=socket_conn,
+                                            msg_to_send=json.dumps(cracked_creds))
+
                 if json.loads(response)["result"] == "Wrong password!":
                     return val_combo
-
-                elif json.loads(response)["result"] == "Wrong login!":
-                    continue
 
             elif creds_type == "password":
-                if json.loads(response)["result"] == "Wrong password!":
-                    continue
-
-                elif json.loads(response)["result"] == "Exception happened during login":
-                    pwd_chars.append(val_combo)
-                    continue
-
+                pwd_chars.append(val_combo)
+                response = socket_operation(socket_connection=socket_conn,
+                                            msg_to_send=json.dumps(cracked_creds))
+                if json.loads(response)["result"] == "Exception happened during login":
+                    check_creds(socket_conn=socket_conn,
+                                creds_data=ALL_ELEMENTS_STR,
+                                creds_type="password")
                 elif json.loads(response)["result"] == "Connection success!":
-                    return val_combo
+                    return
 
-                elif json.loads(response)["result"] == "Bad request!":
-                    continue
+                else:
+                    pwd_chars.pop()
 
 
 def main():
@@ -46,9 +43,9 @@ def main():
         cracked_creds["login"] = check_creds(socket_conn=client_socket,
                                              creds_data=logins,
                                              creds_type="login")
-        cracked_creds["password"] = check_creds(socket_conn=client_socket,
-                                                creds_data=ALL_ELEMENTS_STR,
-                                                creds_type="password")
+        check_creds(socket_conn=client_socket,
+                    creds_data=ALL_ELEMENTS_STR,
+                    creds_type="password")
 
 
 if __name__ == "__main__":
@@ -61,4 +58,4 @@ if __name__ == "__main__":
         "password": "".join(pwd_chars)
     }
     main()
-    print(json.dumps(cracked_creds))
+    print(json.dumps(cracked_creds, indent=4))
