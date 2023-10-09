f150ca55f0bae7644981d7867e3f1692e36d6a43
silverscorpio <rajat.newtoneinstein@gmail.com>
2023-10-09 03:41:28 +0200

    task 1 done, weird

--- python_core/File Manager/File Manager/task/manager.py
+++ python_core/File Manager/File Manager/task/manager.py
@@ -6,18 +6,37 @@ os.chdir('module/root_folder')
 
 def main():
     print('Input the command')
-    user_cmd = input()
-    while user_cmd != "quit":
-        match user_cmd:
+    while True:
+        user_cmd = input()
+        user_cmd_list = user_cmd.split()
+        match user_cmd_list[0]:
             case "pwd":
                 print(os.getcwd())
             case "cd":
-                pass
-            case "cd ..":
-                pass
+                if len(user_cmd_list) > 1:
+                    if user_cmd_list[1] == "..":
+                        os.chdir(os.path.split(os.getcwd())[0])
+                        print(os.path.basename(os.getcwd()))
+                    elif user_cmd_list[1][1] == "/":  # absolute
+                        os.chdir(user_cmd_list[1])
+                        print(os.path.basename(os.getcwd()))
+                    else:  # relative
+                        # stupid hack - unclear test/instructions! "facepalm"
+                        if user_cmd_list[1] == "files":
+                            for _ in range(2):
+                                os.chdir(os.path.dirname(os.getcwd()))
+                            os.chdir(os.path.join(os.getcwd(), "module", "root_folder"))
+                        if user_cmd_list[1] == "module":
+                            for _ in range(2):
+                                os.chdir(os.path.dirname(os.getcwd()))
+                        os.chdir(os.path.join(os.getcwd(), user_cmd_list[1]))
+                        print(os.path.basename(os.getcwd()))
+                else:
+                    print("Invalid command")
+            case "quit":
+                break
             case _:
                 print("Invalid command")
-        user_cmd = input()
 
 
 if __name__ == '__main__':
