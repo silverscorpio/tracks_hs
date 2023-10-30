2a3d3d5fa86d8723628d87f91ee547d1fe627fa3
silverscorpio <rajat.newtoneinstein@gmail.com>
2023-10-30 19:01:03 +0100

    stage 1 done

--- python_core/Learning Progress Tracker (Python)/Learning Progress Tracker (Python)/task/task.py
+++ python_core/Learning Progress Tracker (Python)/Learning Progress Tracker (Python)/task/task.py
@@ -1,1 +1,15 @@
-print("Learning progress tracker")
+def main():
+    print("Learning Progress Tracker")
+    while True:
+        user_input = input().strip()
+        if user_input == "exit":
+            print("Bye!")
+            break
+        elif not user_input:
+            print("No input")
+        else:
+            print("Unknown command!")
+
+
+if __name__ == '__main__':
+    main()
