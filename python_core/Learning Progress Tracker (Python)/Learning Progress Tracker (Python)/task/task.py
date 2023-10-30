3c83875cdba0a3c54450a1e5ab7c28b0d0355e93
silverscorpio <rajat.newtoneinstein@gmail.com>
2023-10-30 21:09:38 +0100

    stage 2: oop based, utils added, plan drawn up

--- python_core/Learning Progress Tracker (Python)/Learning Progress Tracker (Python)/task/task.py
+++ python_core/Learning Progress Tracker (Python)/Learning Progress Tracker (Python)/task/task.py
@@ -1,14 +1,26 @@
+from utils import exit_cmd, add_cmd, back_cmd, no_input
+
+STUDENT_DATA = {}
+
+CMD_DICT = {
+    "exit": exit_cmd,
+    "add students": add_cmd,
+    "back": back_cmd,
+    "no_input": no_input,
+}
+
+
 def main():
     print("Learning Progress Tracker")
     while True:
-        user_input = input().strip()
-        if user_input == "exit":
-            print("Bye!")
-            break
-        elif not user_input:
-            print("No input")
+        user_input = input()
+        if user_input in CMD_DICT:
+            CMD_DICT.get(user_input)()
         else:
-            print("Unknown command!")
+            if not user_input:
+                CMD_DICT.get("no_input")()
+            else:
+                print("Unknown command!")
 
 
 if __name__ == '__main__':
---
+++ python_core/Learning Progress Tracker (Python)/Learning Progress Tracker (Python)/task/utils.py
@@ -0,0 +1,33 @@
+# functions
+
+def exit_cmd():
+    print("Bye!")
+
+
+def no_input():
+    pass
+
+
+def add_cmd():
+    print("Enter student credentials or 'back' to return")
+
+
+def back_cmd():
+    print("Enter 'exit' to exit the program.")
+
+
+# classes
+
+class Student:
+    pass
+
+
+class RegexParser:
+    EMAIL_REGEX = ""
+    NAME_REGEX = ""
+
+    def email_parser(self):
+        pass
+
+    def name_parser(self):
+        pass
