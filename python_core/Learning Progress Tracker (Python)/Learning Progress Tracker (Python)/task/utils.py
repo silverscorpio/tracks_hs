036d5e8f2a3615215cb48f85d2f23eda748cde9c
silverscorpio <rajat.newtoneinstein@gmail.com>
2023-11-03 01:33:22 +0100

    regex class parsing funcs added, major refactor

--- python_core/Learning Progress Tracker (Python)/Learning Progress Tracker (Python)/task/utils.py
+++ python_core/Learning Progress Tracker (Python)/Learning Progress Tracker (Python)/task/utils.py
@@ -43,18 +43,20 @@ def sanitize_input(usr_val: str) -> str:
 class Student:
     student_count: int = 0
 
-    def __new__(cls, name, email):
+    def __new__(cls, first_name, last_name, email):
         cls.student_count += 1
         return super().__new__(cls)
 
-    def __init__(self, name, email):
-        self.name: str = name
+    def __init__(self, first_name, last_name, email):
+        self.first_name: str = first_name
+        self.last_name: str = last_name
         self.email: str = email
         self.id: uuid = uuid.uuid1()
 
     def save_student(self) -> None:
         STUDENT_DATA[self.id] = {
-            "name": self.name,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
             "email": self.email
         }
 
@@ -63,16 +65,21 @@ class Student:
         return Student.student_count
 
     def __str__(self):
-        return f"{self.id}: {self.name}"
+        return f"{self.id} - {self.first_name} {self.last_name}"
 
 
-class RegexParser:
-    EMAIL_REGEX = re.compile(r"[a-zA-Z0-9_\.]+@[a-zA-Z0-9_]+\.[a-z]{2,3}", flags=re.ASCII)
+class InputParser:
+    """
+    extract the email (easier) from the input string and use the match to split and get the name
+    """
     NAME_REGEX = re.compile(r"[A-Za-z' -]+[ '-]{1}[A-Za-z' -]{2,}", flags=re.ASCII)
+    EMAIL_REGEX = re.compile(r"[a-zA-Z0-9_\.]+@[a-zA-Z0-9_]+\.[a-z]{2,3}", flags=re.ASCII)
 
     def __init__(self):
         self._user_input = None
-        self._input_type = None
+        self.email = None
+        self.first_name = None
+        self.last_name = None
 
     @property
     def user_input(self):
@@ -80,40 +87,37 @@ class RegexParser:
 
     @user_input.setter
     def user_input(self, user_input_val):
-        # TODO add check for the value
         self._user_input = user_input_val
 
-    @property
-    def input_type(self):
-        return self._input_type
-
-    @input_type.setter
-    def input_type(self, input_type_val):
-        # TODO add check for the value
-        self._user_input = input_type_val
-
-    @staticmethod
-    def process_regex(regex_type: str, input_str: str):
-        match regex_type:
-            case "name":
-                return RegexParser.match_regex(template=RegexParser.NAME_REGEX, str_to_match=input_str)
-            case "email":
-                return RegexParser.match_regex(template=RegexParser.EMAIL_REGEX, str_to_match=input_str)
-            case _:
-                raise ValueError("Invalid input for Regex Parser")
-
-    @staticmethod
-    def match_regex(template, str_to_match) -> bool:
-        match = re.match(template, str_to_match)
+    def extract_email(self) -> bool | tuple[str, tuple[int, int]]:
+        match = re.search(InputParser.EMAIL_REGEX, self._user_input)
         if match:
-            return True
+            return match.group(), match.span()
         return False
+
+    def process(self):
+        if self._user_input:
+            regex_return = self.extract_email()
+            if regex_return:
+                email, span_indices = regex_return
+                self.email = email.strip()
+                self.first_name, self.last_name = self._user_input[:span_indices(0)].split(maxsplit=1)
+            else:
+                print("No regex match")
+        raise ValueError("User input string needs to be set first")
+
+    def get_data(self) -> dict:
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email
+        }
 
 
 if __name__ == '__main__':
-    x = Student("panda", "chaku@gmail.com")
-    y = Student("chu", "alpha@gmail.com")
-    z = Student("werwer", "psider@gmail.com")
+    x = Student("panda", "dodo", "chaku@gmail.com")
+    y = Student("chu", "champi", "alpha@gmail.com")
+    z = Student("werwer", "lulu", "psider@gmail.com")
     x.save_student()
     y.save_student()
     z.save_student()
