34d65f893d5598a7908742c8c5433398e3437690
silverscorpio <rajat.newtoneinstein@gmail.com>
2023-12-23 04:23:24 +0300

    validator class, utils

---
+++ python_core/Easy Rider Bus Company/Easy Rider Bus Company/task/easyrider/utils.py
@@ -0,0 +1,17 @@
+import json
+
+
+class Validator:
+
+    def __init__(self, json_data: str):
+        self.raw_json = json_data
+        self.json: list = json.loads(self.raw_json)
+
+    def validate_data_types(self):
+        pass
+
+    def validate_required_fields(self):
+        pass
+
+    def get_validation_result(self):
+        pass
