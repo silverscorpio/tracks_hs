f58848174e87fdbae14517f71845f203287f36d0
silverscorpio <rajat.newtoneinstein@gmail.com>
2023-12-26 03:47:26 +0300

    stage 3 init

--- python_core/Easy Rider Bus Company/Easy Rider Bus Company/task/tests.py
+++ python_core/Easy Rider Bus Company/Easy Rider Bus Company/task/tests.py
@@ -1,3 +1,3 @@
-from test.tests import EasyRiderStage2
+from test.tests import EasyRiderStage3
 
-if __name__ == '__main__':    EasyRiderStage2('easyrider.easyrider').run_tests()
+if __name__ == '__main__':    EasyRiderStage3('easyrider.easyrider').run_tests()
