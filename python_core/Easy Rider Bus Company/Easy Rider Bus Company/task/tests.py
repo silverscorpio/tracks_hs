9a8ea82be6b9991b6d91cd61bb9b4ceb9765605f
silverscorpio <rajat.newtoneinstein@gmail.com>
2023-12-26 11:30:10 +0300

    stage 6, last stage init

--- python_core/Easy Rider Bus Company/Easy Rider Bus Company/task/tests.py
+++ python_core/Easy Rider Bus Company/Easy Rider Bus Company/task/tests.py
@@ -1,3 +1,3 @@
-from test.tests import EasyRiderStage5
+from test.tests import EasyRiderStage6
 
-if __name__ == '__main__':    EasyRiderStage5('easyrider.easyrider').run_tests()
+if __name__ == '__main__':    EasyRiderStage6('easyrider.easyrider').run_tests()
