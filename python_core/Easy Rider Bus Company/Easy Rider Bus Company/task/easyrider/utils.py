e605fdb9a10f08f0e8a52e78b23098741138d704
silverscorpio <rajat.newtoneinstein@gmail.com>
2023-12-26 03:18:14 +0300

    stage 2 format errors coded in

--- python_core/Easy Rider Bus Company/Easy Rider Bus Company/task/easyrider/utils.py
+++ python_core/Easy Rider Bus Company/Easy Rider Bus Company/task/easyrider/utils.py
@@ -1,14 +1,22 @@
+import re
 import json
 from collections import defaultdict
+
+
+def match_regex(template: re.Pattern[str], given_str: str) -> bool:
+    match = re.search(template, given_str)
+    if match:
+        return True
+    return False
 
 
 class Validator:
     BUS_ID_REGEX = ""
     STOP_ID_REGEX = ""
-    STOP_NAME_REGEX = ""
+    STOP_NAME_REGEX = re.compile(r"[A-Z]{1}. Road|Avenue|Boulevard|Street", flags=re.ASCII)
     NEXT_STOP_REGEX = ""
-    STOP_TYPE_REGEX = ""
-    A_TIME_REGEX = ""
+    STOP_TYPE_REGEX = re.compile(r"[SOF]{1}", flags=re.ASCII)
+    A_TIME_REGEX = re.compile(r"^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$", flags=re.ASCII)
 
     def __init__(self, json_data: str):
         self.raw_json = json_data
@@ -40,15 +48,25 @@ class Validator:
         self.stop_type_validator()
         self.a_time_validator()
 
-    def report_errors(self):
+    def report_errors(self, format_errors: False):
         total_type_errors = sum([v["type"] for v in self.errors.values()])
         total_required_errors = sum([v["required"] for v in self.errors.values()])
+        total_format_errors = sum([v["format"] for v in self.errors.values()])
 
         field_errors = defaultdict(int)
+
         for k, v in self.errors.items():
             field_errors[k] = sum([v["type"], v["required"]])
 
-        print(f"""
+        if format_errors:
+            print(f"""
+Format validation: {total_format_errors} errors
+stop_name: {self.errors["stop_name"]["format"]}
+stop_type: {self.errors["stop_type"]["format"]}
+a_time: {self.errors["a_time"]["format"]}""")
+
+        else:
+            print(f"""
 Type and required field validation: {total_type_errors + total_required_errors} errors
 bus_id: {field_errors["bus_id"]}
 stop_id: {field_errors["stop_id"]}
@@ -79,11 +97,17 @@ a_time: {field_errors["a_time"]}""")
     def stop_name_validator(self):
         # string
         for val in self.stop_names:
+            # field errors
             if val[1] and not isinstance(val[1], str):
                 self.errors["stop_name"]["type"] += 1
 
             if val[1] == "":
                 self.errors["stop_name"]["required"] += 1
+
+            # format errors
+            stop_name_match = match_regex(template=Validator.STOP_NAME_REGEX, given_str=val[1])
+            if not stop_name_match:
+                self.errors["stop_name"]["format"] += 1
 
     def next_stop_validator(self):
         # integer
@@ -98,8 +122,14 @@ a_time: {field_errors["a_time"]}""")
         # char (string of len 1)
         for val in self.stop_types:
             if val[1]:
+                # field type errors
                 if (not isinstance(val[1], str)) or (isinstance(val[1], str) and len(val[1]) != 1):
                     self.errors["stop_type"]["type"] += 1
+
+                # format errors
+                stop_name_match = match_regex(template=Validator.STOP_NAME_REGEX, given_str=val[1])
+                if not stop_name_match:
+                    self.errors["stop_type"]["format"] += 1
 
     def a_time_validator(self):
         # string
@@ -109,3 +139,8 @@ a_time: {field_errors["a_time"]}""")
 
             if val[1] == "":
                 self.errors["a_time"]["required"] += 1
+
+            # format errors
+            arrival_time_match = match_regex(template=Validator.A_TIME_REGEX, given_str=val[1])
+            if not arrival_time_match:
+                self.errors["a_time"]["format"] += 1
